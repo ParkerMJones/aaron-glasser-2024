7edb2828405e2a7072693f13802e0cafdf49d4a3
Parker Jones <parker.matthewjones@gmail.com>
2024-07-05 22:31:33 -0500

    fix curated id page

--- app/routes/movies.curated.$id.tsx
+++ app/routes/movies.curated.$id.tsx
@@ -1,7 +1,8 @@
-import { useParams } from "@remix-run/react";
+import { Link, useParams } from "@remix-run/react";
 import { imgArr } from "~/utils/image-array";
 import { curatedPrograms } from "~/utils/programs";
 import invariant from "tiny-invariant";
+import { ArrowLeft } from "react-feather";
 
 export default function SelectedProgram() {
   const param = useParams();
@@ -9,18 +10,25 @@ export default function SelectedProgram() {
   const selectedProgram = Number(param.id);
 
   return (
-    <div className="m-9 ml-3 sm:ml-14 max-w-[100ch]">
-      <h2 className="pb-4 font-bold">
+    <div className="mt-12 pb-24 px-6 sm:ml-14 max-w-[100ch] text-soft-white flex flex-col gap-y-10">
+      <Link
+        to="/movies/curated"
+        className="text-[#f0f0f0] hover:underline underline-offset-4 flex items-center gap-x-2"
+      >
+        <ArrowLeft size={24} />
+        Back
+      </Link>
+      <h2 className="font-bold mt-4">
         {curatedPrograms[selectedProgram].title}
       </h2>
-      <div className="my-3 mx-0">
+      <div>
         <img
           src={imgArr[selectedProgram]}
           alt={curatedPrograms[selectedProgram].title}
           style={{ width: 400, maxWidth: "100%" }}
         />
       </div>
-      <p className="px-3 py-0 leading-6 max-w-[90ch]">
+      <p className="leading-6 max-w-[90ch]">
         {curatedPrograms[selectedProgram].description}
       </p>
       <h4>PROGRAM</h4>
@@ -29,16 +37,14 @@ export default function SelectedProgram() {
           <div key={program}>{program}</div>
         ))}
       </li>
-      <div style={{ padding: "12px 0" }}>-----------------------</div>
+      <div>-----------------------</div>
       <li style={{ listStyle: "none", lineHeight: "1.4rem" }}>
         {curatedPrograms[selectedProgram].secondHalf.map((program) => (
           <div key={program}>{program}</div>
         ))}
       </li>
-      <div style={{ paddingTop: 12 }}>
-        {curatedPrograms[selectedProgram].notes}
-      </div>
-      <div style={{ paddingTop: 16 }}>
+      <div>{curatedPrograms[selectedProgram].notes}</div>
+      <div>
         Screened at The Bridge Progressive Arts Initiative on{" "}
         {curatedPrograms[selectedProgram].date},{" "}
         {curatedPrograms[selectedProgram].year}
